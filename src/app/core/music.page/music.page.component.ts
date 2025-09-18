387ded468fba6e8aa996cb6c6311b6afad56a98f
Steven Nguyen <shnguy03@louisville.edu>
2025-09-18 03:29:13 -0400

    Music PDFs

--- src/app/core/music.page/music.page.component.ts
+++ src/app/core/music.page/music.page.component.ts
@@ -1,10 +1,11 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { SharedModule } from '../../shared/shared-module';
 
 @Component({
   selector: 'app-music-page',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './music.page.component.html',
   styleUrls: ['./music.page.component.scss'],
 })
@@ -12,9 +13,16 @@ export class MusicPageComponent implements AfterViewInit {
   pdfFiles: { name: string; path: SafeResourceUrl }[] = [];
 
   private rawFiles = [
-    { name: 'Sheet 1', path: 'assets/pdfs/Autumn Leaves.pdf' },
-    { name: 'Sheet 2', path: 'assets/pdfs/Caprice in D Minor.pdf' },
-    { name: 'Sheet 3', path: 'assets/pdfs/Mariage_d\'Amour.pdf' },
+    { name: 'Autumn Leaves', path: 'assets/pdfs/Autumn Leaves.pdf' },
+    { name: 'Caprice in D Minor', path: 'assets/pdfs/Caprice in D Minor by Matteo Carcassi.pdf' },
+    { name: 'Mariage d\'Amour', path: 'assets/pdfs/Mariage_d\'Amour.pdf' },
+    { name: 'Gymnopédie No.1', path: 'assets/pdfs/Gymnopedie.pdf' },
+    { name: 'Gymnopédie No.1', path: 'assets/pdfs/Prelude 1.pdf' },
+    { name: 'Rumba Flamenco by Romano', path: 'assets/pdfs/Rumba Flamenco by Romano.pdf' },
+    { name: 'Danza Brasiliera', path: 'assets/pdfs/Danza Brasília.pdf' },
+    { name: 'Fly me to the moon', path: 'assets/pdfs/Fly me to the moon.pdf' },
+    { name: 'Paul Anka', path: 'assets/pdfs/Paul Anka Tabs.pdf' },
+    { name: 'Fly me to the moon', path: 'assets/pdfs/Recuerdos de la Alhambra.pdf' }    
   ];
 
   constructor(private sanitizer: DomSanitizer) {}
